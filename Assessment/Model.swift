8eb166bcacfd6214e662388d185fdb96c697d53f
BatyrOfLife <batyr@lifeunites.us>
2025-02-07 10:43:20 -0600

    Add Model

---
+++ Assessment/Model.swift
@@ -0,0 +1,39 @@
+//
+//  Model.swift
+//  Assessment
+//
+//  Created by Batyr Ovezdurdyyev on 2/7/25.
+//
+
+import SwiftUI
+
+struct ResponseModel: Codable {
+    let title: String
+    let link: String
+    let description: String
+    let modified: Date
+    let generator: String
+    let items: [Item]
+}
+
+struct Item: Codable {
+    let title: String
+    let link: String
+    let media: Media
+    let dateTaken: Date
+    let description: String
+    let published: Date
+    let author, authorID, tags: String
+
+    enum CodingKeys: String, CodingKey {
+        case title, link, media
+        case dateTaken = "date_taken"
+        case description, published, author
+        case authorID = "author_id"
+        case tags
+    }
+}
+
+struct Media: Codable {
+    let m: String
+}

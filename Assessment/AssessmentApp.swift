a6a896a06cae0773be6f18c2a7b2b945ba9c4ada
BatyrOfLife <batyr@lifeunites.us>
2025-02-07 10:34:37 -0600

    Initial Commit

---
+++ Assessment/AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssessmentApp.swift
+//  Assessment
+//
+//  Created by Batyr Ovezdurdyyev on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
